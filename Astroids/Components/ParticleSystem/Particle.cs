913971d6329e9119510277c2e56594362fca5708
DreLnBrown <DreLnBrown@4a15b48e-0d9c-11df-a8f6-f3887e771010>
2010-01-30 17:08:27 +0000

    - added particle system

---
+++ Astroids/Components/ParticleSystem/Particle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids.Components.ParticleSystem
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class Particle
+    {
+        private Vector2 position;
+        private float startTime = 0.0f;
+        Color color;
+        bool active;
+
+        public float StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public Particle()
+        {
+            position = new Vector2();
+            color = new Color();
+            active = false;
+        }
+
+        public void Update( float tick )
+        {
+            startTime += tick;
+        }
+    }
+}
---
+++ Astroids/Components/ParticleSystem/ParticleEmitter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids.Components.ParticleSystem
+{
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework;
+
+    public abstract class ParticleEmitter
+    {
+        private Color startColor,endColor;
+        private Vector2 velocity,position;
+     
+        private Texture2D image;
+        private float particleTime;
+        private int particleCount;
+        private int particleIndex;
+        private Particle[] particles;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public ParticleEmitter( 
+            Texture2D particleImage_,
+            float particleTime_,
+            Color startColor_,
+            Color endColor_ ,
+            Vector2 velocity_,
+            int particleCount_ )
+        {
+            particleTime = particleTime_;
+            image = particleImage_;
+            startColor = startColor_;
+            endColor = endColor_;
+            velocity = velocity_;
+            particleCount = particleCount_;
+            particleIndex = 0;
+
+            position = new Vector2();
+            particles = new Particle[particleCount];
+        }
+
+        protected virtual void ActivateParticle( Particle particle )
+        {
+            particle.Active = true;
+            particle.Color = startColor;
+            particle.Position = position;
+            particle.StartTime = 0.0f;
+        }
+
+        public void Emit(int count)
+        {
+            int particleSearcCount = 0;
+            int particlesAdded = 0;
+            while( particleSearcCount < particleCount 
+                   || particlesAdded == count )
+            {
+                if( particles[ particleIndex ].Active == false )
+                {
+                    ++particlesAdded;
+                    ActivateParticle(particles[particleIndex]);
+
+                }
+
+                ++particleSearcCount;
+                ++particleIndex;
+
+                if (particleIndex >= particleCount) particleIndex = 0;
+            }
+        }
+
+        public abstract void Update(GameTime time_);
+        public abstract void Draw(GameTime time_);
+    }
+}
