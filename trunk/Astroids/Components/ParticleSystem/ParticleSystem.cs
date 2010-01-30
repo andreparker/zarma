3e04f8de1e56028c1b784f6295f2cab901c0c35a
DreLnBrown <DreLnBrown@4a15b48e-0d9c-11df-a8f6-f3887e771010>
2010-01-30 17:08:27 +0000

    - added particle system

---
+++ trunk/Astroids/Components/ParticleSystem/ParticleSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids.Components.ParticleSystem
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    class ParticleSystem : DrawableGameComponent
+    {
+        SpriteBatch spriteBatch;
+
+        ParticleSystem( Game game_, SpriteBatch spriteBatch_):
+            base( game_ )
+        {
+            spriteBatch = spriteBatch_;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
